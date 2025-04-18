095a5336481c3f6292fd829a924197c5964c3658
svalenelatis <tacomoyes123@gmail.com>
2025-04-18 07:27:23 -0600

    Server and Data Objects Changes

--- server/server.js
+++ server/server.js
@@ -2,11 +2,29 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dbHandler = require('./dbHandler');
 const TransactionManager = require('./transactionManager');
+const cors = require('cors')
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const corsOptions = {
+    origin: '*', // Allow all origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204 // Some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions)); // Enable CORS for all routes
+
+
 app.use(bodyParser.json());
+
+//log all attempted requests to the console
+app.use((req, res, next) => {
+    console.log(`Request: ${req.method} ${req.url}`);
+    next();
+}
+);
 
 const transactionManager = new TransactionManager();
 
@@ -40,6 +58,105 @@ app.get('/api/prices', async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
+
+app.get('/api/player'), async (req, res) => { //MAKE SURE THE BODY OF THE REQUEST CONTAINS A PLAYER NAME OR THIS ROUTE WONT WORK
+    try {
+
+
+        item = {}
+        if(req.body.searchName === "Synn"){
+            item = {
+                "name" : "Synn Rixen",
+                "home port" : "Katu",
+                "Gold" : 1400,
+                "Inventory" : {
+                    "Grain" : 10,
+                    "Stone" : 10,
+                    "Fruit" : 10
+                },
+                "Ships" : {
+                    "Ship Name" : {
+                        "Speed" : 1,
+                        "Inventory" : {
+                            "Iron" : 3
+                        },
+                        "Status" : "Ready"
+                    },
+                    "Ship Name 2" : {
+                        "Speed" : 2,
+                        "Inventory" : {
+                            "Preserves" : 3
+                        },
+                        "Status" : "Returning"
+                    }
+                },
+                "Factories" : {
+                    "Iron Mine" : {
+                        "Price" : {
+
+                        },
+                        "Production" : {
+                            "Iron Ore" : 1
+                        }
+                    }
+                }
+            }
+        }
+        else if(req.body.searchName == "Rad'num"){
+            item = {
+                "name" : "Rad'num LongLastname",
+                "home port" : "Greenlands",
+                "Gold" : 200,
+                "Inventory" : {
+                    "Grain" : 2,
+                    "Stone" : 5,
+                    "Fruit" : 2
+                },
+                "Ships" : {
+                    "Ship Name" : {
+                        "Speed" : 1,
+                        "Inventory" : {
+                            "Coal" : 9
+                        },
+                        "Status" : "Not Ready"
+                    },
+                    "Ship Name 2" : {
+                        "Speed" : 1.5,
+                        "Inventory" : {
+                            "Jewelry" : 1
+                        },
+                        "Status" : "Ready"
+                    }
+                },
+                "Factories" : {
+                    "Coal Mine" : {
+                        "Price" : {
+
+                        },
+                        "Production" : {
+                            "Coal" : 1
+                        }
+                    },
+                    "Rations Factory" : {
+                        "Price" : {
+                            "Flour" : 1,
+                            "Livestock" : 1,
+                            "Water" : 1
+                        },
+                        "Production" : {
+                            "Rations" : 2
+                        }
+                    }
+                }
+            }
+        }
+        
+        res.json(item);
+    } catch (e) {
+        console.error('Error fetching prices:', e);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+}
 
 // app.post('/api/signup', async (req, res) => {
 //     try {
